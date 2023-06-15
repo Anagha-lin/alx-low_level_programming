e40a85a0f197023dd1fdca1e7e2f91ff00a6e2a7
Anagha-lin <linusanaha37@gmail.com>
2023-06-15 11:52:09 -0700

    put c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <studio.h>
+
+/**
+ * main - Entry point
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	puts("/"programming is like a multilingual puzzle");
+	return (0);
+}
