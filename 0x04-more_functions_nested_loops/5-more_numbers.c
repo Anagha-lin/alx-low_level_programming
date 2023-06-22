ba1ec1d630d5ea3b0dd2a7acbb3848165d9ae876
Anagha-lin <linusanaha37@gmail.com>
2023-06-22 13:13:52 -0700

    more number

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,27 +1,20 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
-  * more_numbers - Print 10 times the numbers since 0 up to 14
-  *
-  * Return: 10 times of the numbers since 0 up to 14
-  */
-void more_numbers(void)
+ * main - Prints all single digit numbers of base 10 starting from 0,
+ *        only using putchar and without char variables.
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
-	int x, y;
+	int num;
 
-	for (x = 0; x < 10; x++)
-	{
-		for (y = 0; y <= 14; y++)
-		{
-			if (y > 9)
-			{
-				_putchar((y / 10) + '0');
-			}
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
 
-			putchar((y % 10) + '0');
-		}
+	putchar('\n');
 
-		putchar('\n');
-	}
+	return (0);
 }
-
