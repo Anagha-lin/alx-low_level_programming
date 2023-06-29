ca4e3e3a7858bd01ad97a1a4449b9ba0816677b9
Anagha-lin <linusanaha37@gmail.com>
2023-06-29 10:35:52 -0700

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,29 @@
+#include "stdio.h"
+/**
+ * print_number - prints an arbitrary integer using putchar
+ *
+ * @n: integer to print
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	unsigned int u, i;
+
+	if (n < 0)
+	{
+		u = -n;
+		putchar('-');
+	}
+	else
+	{
+		u = n;
+	}
+
+	i = 1000000000;
+	do {
+		if (i <= u || i == 1)
+			putchar(u / i % 10 + '0');
+		i /= 10;
+	} while (i != 0);
+}
