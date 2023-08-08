1ef7c91cf7e5251995c5dfceadc5f296b25fb942
Anagha-lin <linusanaha37@gmail.com>
2023-08-08 05:04:51 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,14 +15,14 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 	{
 		result = -1;
-		return (1);
+		return (result);
 	}
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 	{
 		result = -1;
-		return (1);
+		return (result);
 	}
 
 	if (text_content)
@@ -36,5 +36,5 @@ int create_file(const char *filename, char *text_content)
 			result = -1;
 	}
 	close(fd);
-	return (1);
+	return (result);
 }
